b91d8fd5609064348f9472353436f22be46b7faf
FarhanDavinRinaldi <farhandavin14@gmail.com>
2025-11-27 15:18:41 +0700

    add vercel config to server

--- clients/src/App.jsx
+++ clients/src/App.jsx
@@ -22,7 +22,7 @@ function App() {
     }
 
     try {
-      const response = await fetch("http://localhost:5000/api/buat-kuis", {
+      const response = await fetch("https://web-belajar-ai-tekt.vercel.app", {
         method: "POST",
         body: formData,
       });
